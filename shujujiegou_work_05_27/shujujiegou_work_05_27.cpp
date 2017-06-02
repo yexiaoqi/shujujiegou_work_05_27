83af046a07eba62653d1423765630a02952bf095
yexiaoqi <2417192079@qq.com>
2017-06-02 19:14:43 +0800

    修改1

--- shujujiegou_work_05_27/shujujiegou_work_05_27.cpp
+++ shujujiegou_work_05_27/shujujiegou_work_05_27.cpp
@@ -2,11 +2,29 @@
 //
 
 #include "stdafx.h"
-
 template<class T>
-
-int main()
+class BinaryTree<T>
 {
-    return 0;
+	int Get_num_onedegree(BinTreeNode<T>*subtree);
+};
+template<class T>
+int BinaryTree<T>:: Get_num_onedegree(BinTreeNode<T>*subtree)
+{
+	if (subtree == 0)
+	{
+		return NULL;
+	}
+	if (subtree->leftchild == 0 && subtree->rightchild != 0)
+	{
+		return 1 + Get_num_onedegree(subtree->rightchild);
+	}
+	if (subtree->rightchild == 0 && subtree->leftchild != 0)
+	{
+		return 1 + Get_num_onedegree(subtree->leftchild);
+	}
+	else
+	{
+		return Get_num_onedegree(subtree->rightchild)+ Get_num_onedegree(subtree->leftchild);
+	}
 }
 
